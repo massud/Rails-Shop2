542bf7937255b4792e2dca23a8ada8118441743e
massud <massudgnan@gmail.com>
2015-06-08 12:27:29 +0100

    enable users to place orders

---
+++ app/models/order.rb
@@ -0,0 +1,7 @@
+class Order < ActiveRecord::Base
+  validates :address, :city, :postcode, presence: true
+
+  belongs_to :listing
+  belongs_to :buyer, class_name: "User"
+  belongs_to :seller, class_name: "User"
+end
